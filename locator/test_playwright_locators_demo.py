c5c89d81a9482056cca541a41497864e1f4193ea
openthedidi <openthedidi2004@gmail.com>
2026-04-05 15:00:18 +0800

    playwright內建locator p2

--- locator/test_playwright_locators_demo.py
+++ locator/test_playwright_locators_demo.py
@@ -6,12 +6,25 @@ def test_pwlocators(page: Page):
     page.wait_for_timeout(3000)
 
     # get_by_alt_text
-    logo = page.get_by_alt_text("nopCommerce demo store")
-    expect(logo).to_be_visible()
+    # logo = page.get_by_alt_text("nopCommerce demo store")
+    # expect(logo).to_be_visible()
 
-    # get_by_text()
-    expect(page.get_by_text("Welcome to our store")).to_be_visible()
-    expect(page.get_by_text("Welcome to")).to_be_visible()
+    # # get_by_text()
+    # expect(page.get_by_text("Welcome to our store")).to_be_visible()
+    # expect(page.get_by_text("Welcome to")).to_be_visible()
 
-    # get_by_role()
-    expect(page.get_by_role("button", name="Computers")).to_be_visible()
+    # # get_by_role() : role對應要看官方對應表
+    # expect(page.get_by_role("button", name="Computers")).to_be_visible()
+
+    # get_by_label #label顯示給user的名稱
+    page.get_by_label("Excellent").set_checked(True)
+    page.wait_for_timeout(3000)
+
+    # get_by_placeholder()
+    page.get_by_placeholder("Search store").fill("Apple MacBook Pro 13-inch")
+    page.wait_for_timeout(3000)
+
+    # get_by_title()
+    expect(page.get_by_title("Show products in category Electronics")
+           ).to_have_text("Electronics")
+    page.wait_for_timeout(3000)
